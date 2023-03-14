cb33b33855641142dd0351d17d90a4805ea8ac52
Liana Sabino Alves <liana.sabino@matriz.fornecedoramaquinas.com.br>
2023-03-14 13:54:49 -0300

    Estrutura visual

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
-import { Content, Overlay } from './styles';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal() {
     return (
@@ -10,17 +11,31 @@ export function NewTransactionModal() {
     <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
 
+        <CloseButton>
+            <X size={24}/>
+        </CloseButton>
+
         <form action="">
             <input type="text" placeholder="Descrição" required />
             <input type="number" placeholder="Preço" required />
             <input type="text" placeholder="Categoria" required />
 
+            <TransactionType>
+                <TransactionTypeButton variant='income' value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                </TransactionTypeButton>
+            
+                <TransactionTypeButton variant='outcome' value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                </TransactionTypeButton>
+            </TransactionType>
+
             <button type="submit">
                 Cadastrar
             </button>
         </form>
-
-        <Dialog.Close />
     </Content>
 </Dialog.Portal>
     )
